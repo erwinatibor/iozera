a40efbccde15db73c8dcf0b0c1a6a7008a1c340d
erwinatibor <erwinatibor@gmail.com>
2026-04-01 08:09:29 +0800

    Delete docs directory

--- docs/_nuxt/DLtQiEbb.js
+++
@@ -1,2 +0,0 @@
-const e=n=>n.split(`
-`).filter(t=>!!t).map(t=>`<span>${t}</span>`).join("")??"",a=n=>n?.replaceAll("{{","<span>").replaceAll("}}","</span>")??"",s=n=>n?.content?.length?n.content.some(t=>t.type==="paragraph"?t.content&&t.content.length>0:!0):!1,r=(n,t="_blank")=>({url:n,linktype:"url",target:t});export{s as a,a as h,e as s,r as t};
